d2acfa066e3dda4b18c63148c157df727b39d2d9
Darwinpz <dpilaloazea@gmail.com>
2024-02-25 18:05:55 -0500

    Base del proyecto

---
+++ app/src/main/java/com/darwinpz/asistencia/Fragmentos/Fragment_Inicio.java
@@ -0,0 +1,26 @@
+package com.darwinpz.asistencia.Fragmentos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.darwinpz.asistencia.R;
+
+public class Fragment_Inicio extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View vista = inflater.inflate(R.layout.fragment_inicio, container, false);
+
+        return vista;
+
+    }
+}
---
+++ app/src/main/java/com/darwinpz/asistencia/Fragmentos/Fragment_Usuarios.java
@@ -0,0 +1,26 @@
+package com.darwinpz.asistencia.Fragmentos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.darwinpz.asistencia.R;
+
+public class Fragment_Usuarios extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_usuarios,container,false);
+
+
+        return vista;
+
+    }
+}
